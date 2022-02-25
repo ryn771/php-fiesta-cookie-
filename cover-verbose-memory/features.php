3780c132afd7d51564a97f166c511db985d67262
Mark Ryan O. Gabuco <99346414+ryn771@users.noreply.github.com>
2022-02-25 09:38:03 +0800

    inc directory done

---
+++ cover-verbose-memory/features.php
@@ -0,0 +1,35 @@
+<?php include'inc/header.php' ?>
+
+<main role="main" class="inner cover">
+        <h1 class="cover-heading">Features.</h1>
+        <table class="table table-dark table-hover">
+        <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+        </table>
+      </main>
+<?php include'inc/footer.php' ?>
